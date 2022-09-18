ca50ab2f03b410ac93289520fb6c2769cc9c2627
Bruno Samuel <brunosamuel.orizona@gmail.com>
2022-09-18 15:40:41 -0300

    Created classes Board and Piece - not completed

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,10 @@
 package application;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Program {
 	public static void main(String[] args) {
-		Position p = new Position(8, 5);
-		
-		System.out.println(p);
+		Board board = new Board(8, 8);
 
 	}
 }
